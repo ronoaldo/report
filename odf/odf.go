3196eaed5e877a2ecb3c1ce73148cc30ab54fe05
Ronoaldo JLP <ronoaldo@gmail.com>
2018-02-01 21:53:20 -0200

    Initial working implementation for ODT and ODG.

---
+++ odf/odf.go
@@ -0,0 +1,219 @@
+package odf
+
+import (
+	"archive/zip"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/beevik/etree"
+)
+
+var errNotImplemented = errors.New("odf: not impemented")
+
+// OpenDocument represents an open document reader and writter.
+type OpenDocument struct {
+	zipFile *os.File
+	cache   map[string]*bytes.Buffer
+}
+
+// Open loads the file contents in-memory
+func Open(filename string) (*OpenDocument, error) {
+	r, err := zip.OpenReader(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	odf := &OpenDocument{}
+	odf.initCache()
+
+	for _, f := range r.File {
+		logger.Printf("archive=%v; file=%v; dir=%v", filename, f.Name, f.FileInfo().IsDir())
+		if f.FileInfo().IsDir() {
+			continue
+		}
+		buff := odf.fileBuffer(f.Name)
+		zipfd, err := f.Open()
+		if err != nil {
+			logger.Printf("Error opening file %v from zip: %v", f, err)
+			continue
+		}
+		w, err := io.Copy(buff, zipfd)
+		if err != nil {
+			logger.Printf("Error reading file %v from zip: %v", f, err)
+			continue
+		}
+		if uint64(w) != f.UncompressedSize64 {
+			logger.Printf("Error reading file: not all bytes read, expected %d, got %d", f.UncompressedSize64, w)
+		}
+		logger.Printf("Read %d bytes from %v", w, f.Name)
+	}
+	return odf, nil
+}
+
+// Execute merges the provided data into all loaded and parsed template files.
+func (o *OpenDocument) Execute(values interface{}) error {
+	filesToMerge := []string{"content.xml", "styles.xml"}
+	for _, fname := range filesToMerge {
+		f := o.fileBuffer(fname)
+		xml := f.String()
+		if fname == "content.xml" {
+			var err error
+			xml, err = prepareXMLForTemplate(xml)
+			if err != nil {
+				return err
+			}
+		}
+		tpl, err := template.New(fname).Parse(xml)
+		if err != nil {
+			return err
+		}
+
+		buff := &bytes.Buffer{}
+		if err = tpl.Execute(buff, values); err != nil {
+			return err
+		}
+		f.Reset()
+		b, err := io.Copy(f, buff)
+		if err != nil {
+			return err
+		}
+		logger.Printf("Merged %s (%d bytes)", fname, b)
+	}
+	return nil
+}
+
+// WriteFile package and write the file contents into the destination.
+func (o *OpenDocument) WriteFile(filename string) error {
+	fd, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	w := zip.NewWriter(fd)
+	defer fd.Close()
+
+	// We need to overwrite each zip file
+	for k, buff := range o.cache {
+		zipfd, err := w.Create(k)
+		if err != nil {
+			return err
+		}
+		b, err := io.Copy(zipfd, buff)
+		if err != nil {
+			return err
+		}
+		logger.Printf("Written %d bytes for %s into zip.", b, k)
+	}
+	if err := w.Flush(); err != nil {
+		return err
+	}
+	return w.Close()
+}
+
+// Extract decodes the file and extracts it's content into the specified folder
+func (o *OpenDocument) Extract(folder string) error {
+	return errNotImplemented
+}
+
+func (o *OpenDocument) initCache() {
+	o.cache = make(map[string]*bytes.Buffer)
+}
+
+func (o *OpenDocument) listFiles() (files []string) {
+	for k := range o.cache {
+		files = append(files, k)
+	}
+	return
+}
+
+func (o *OpenDocument) fileBuffer(filename string) *bytes.Buffer {
+	if b, ok := o.cache[filename]; ok {
+		return b
+	}
+	b := &bytes.Buffer{}
+	o.cache[filename] = b
+	return b
+}
+
+func prepareXMLForTemplate(rawXML string) (string, error) {
+	// Parse XML file using DOM
+	doc := etree.NewDocument()
+	err := doc.ReadFromString(rawXML)
+	if err != nil {
+		return rawXML, err
+	}
+
+	// Search for paragraphs and fix up invalid elements
+	toClean := []string{"//text:p", "//annotation"}
+	for _, path := range toClean {
+		for _, p := range doc.FindElements(path) {
+			var prev *etree.Element
+			spans := p.ChildElements()
+			for i := range spans {
+				s := spans[i]
+				if s.Tag != "span" {
+					continue
+				}
+				if s.Tag == "s" {
+					logger.Printf("Removed <text:s/>")
+					p.RemoveChild(s)
+					continue
+				}
+				// Let's clean up some dup styles that libreoffice goes on crazy building.
+				if prev == nil {
+					prev = s
+					continue
+				}
+
+				if s.SelectAttr("style-name").Value == prev.SelectAttr("style-name").Value {
+					logger.Printf("Found matching style from previous tag. merge in the contents")
+					prev.SetText(prev.Text() + s.Text())
+					p.RemoveChild(s)
+				}
+			}
+		}
+	}
+
+	// Remove annotations, and move the contents to up/bottom of table row
+	for _, a := range doc.FindElements("//annotation") {
+		content := ""
+		for _, e := range a.ChildElements() {
+			if e.Tag == "p" {
+				content = e.SelectElement("span").Text()
+			}
+		}
+		if strings.Contains(content, "range") || strings.Contains(content, "end") {
+			tr := a.Parent()
+			for tr.Tag != "body" && tr.Tag != "table-row" {
+				tr = tr.Parent()
+			}
+			if tr.Tag == "body" {
+				return rawXML, fmt.Errorf("odf: found annotation with %s but no parent table-row found", content)
+			}
+			table := tr.Parent()
+			textNode := etree.NewCharData(content)
+
+			if strings.Contains(content, "range") {
+				table.InsertChild(tr, textNode)
+			} else {
+				table.AddChild(textNode)
+			}
+		}
+		a.Parent().RemoveChild(a)
+	}
+
+	// Render new XML
+	cleanXML, err := doc.WriteToString()
+	if err != nil {
+		return rawXML, err
+	}
+
+	// Quick and dirty fix to allow us to use '""' and '&' inside template directives
+	cleanXML = strings.Replace(cleanXML, "&quot;", "\"", -1)
+	return cleanXML, err
+}
---
+++ odf/odf_test.go
@@ -0,0 +1,163 @@
+package odf
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var (
+	odfFile, dataFile, outFile string
+)
+
+func init() {
+	flag.StringVar(&odfFile, "odf", "testdata/report.odt", "`Source odf file` to run the test against")
+	flag.StringVar(&dataFile, "data", "testdata/data.json", "`Data file` to use as input")
+	flag.StringVar(&outFile, "out", "testdata/out.odt", "`Destination file` to use as output. WARNING: will be overwritten")
+}
+
+func TestExecuteTemplate(t *testing.T) {
+	odf, err := Open(odfFile)
+	if err != nil {
+		t.Fatalf("Error opening file: %v", err)
+	}
+
+	// Check if buffers where read
+	t.Logf("DEBUG: odf.cache: %#v", odf.listFiles())
+	for _, f := range []string{"content.xml", "styles.xml"} {
+		fd := odf.fileBuffer(f)
+		if fd.Len() == 0 {
+			t.Errorf("Unexpected nil zipfile inside ODF struct: %v", f)
+		}
+		// Check if prepare will work as expected
+		rawXML := fd.String()
+		cleanXML, err := prepareXMLForTemplate(rawXML)
+		if err != nil {
+			t.Errorf("Unable to prepare XML for template: %v", err)
+		}
+		if err := ioutil.WriteFile("/tmp/"+f, []byte(cleanXML), 0644); err != nil {
+			t.Errorf("Error generating temp file %v", err)
+		}
+	}
+
+	// Execute with data
+	var data map[string]interface{}
+	if data, err = loadJSONFile(dataFile); err != nil {
+		t.Fatalf("Failed to load data file %v", err)
+	}
+	items := make([]interface{}, 0)
+	gt := 0.0
+	for i := 0; i < 100; i++ {
+		v := float64(i * 3 % 4)
+		q := i * 2 % 10
+		item := map[string]interface{}{
+			"ItemNo": i,
+			"Name":   fmt.Sprintf("Random item #%d", i),
+			"Quant":  q,
+			"Value":  fmt.Sprintf("$ %.02f", v),
+			"Total":  fmt.Sprintf("$ %.02f", v*float64(q)),
+		}
+		items = append(items, item)
+		gt += v
+	}
+	data["Items"] = items
+	data["GrandTotal"] = fmt.Sprintf("$ %.2f", gt)
+
+	if err = odf.Execute(data); err != nil {
+		t.Fatalf("Error executing odf template: %v", err)
+	}
+
+	if err = odf.WriteFile(outFile); err != nil {
+		t.Fatalf("Error saving file")
+	}
+}
+
+func loadJSONFile(fname string) (map[string]interface{}, error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make(map[string]interface{})
+	if err := json.NewDecoder(f).Decode(&res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func xTestMergeTags(t *testing.T) {
+	input := `<?xml version="1.0" encoding="UTF-8"?>
+<office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:presentation="urn:oasis:names:tc:opendocument:xmlns:presentation:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:smil="urn:oasis:names:tc:opendocument:xmlns:smil-compatible:1.0" xmlns:anim="urn:oasis:names:tc:opendocument:xmlns:animation:1.0" xmlns:rpt="http://openoffice.org/2005/report" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:css3t="http://www.w3.org/TR/css3-text/" office:version="1.2">
+<draw:frame draw:style-name="gr25" draw:text-style-name="P4" draw:layer="layout" svg:width="6.852cm" svg:height="0.31cm" svg:x="6.102cm" svg:y="9.568cm">
+  <draw:text-box>
+    <text:p>
+	  <text:span text:style-name="T7">{{or .</text:span>
+	  <text:span text:style-name="T7">Mar }</text:span>
+	  <text:span text:style-name="T7">}</text:span>
+	</text:p>
+  </draw:text-box>
+</draw:frame>`
+	t.Logf("input=%s", input)
+
+	output, err := prepareXMLForTemplate(input)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	t.Logf("*** output= %v", output)
+}
+
+func xTestParseAnnotations(t *testing.T) {
+	input := ` <office:body>
+	<office:text>
+	 <text:sequence-decls>
+	  <text:sequence-decl text:display-outline-level="0" text:name="Illustration"/>
+	  <text:sequence-decl text:display-outline-level="0" text:name="Table"/>
+	  <text:sequence-decl text:display-outline-level="0" text:name="Text"/>
+	  <text:sequence-decl text:display-outline-level="0" text:name="Drawing"/>
+	 </text:sequence-decls>
+	 <table:table table:name="Tabela1" table:style-name="Tabela1">
+	  <table:table-column table:style-name="Tabela1.A"/>
+	  <table:table-column table:style-name="Tabela1.B"/>
+	  <table:table-column table:style-name="Tabela1.C"/>
+	  <table:table-column table:style-name="Tabela1.D"/>
+	  <table:table-column table:style-name="Tabela1.E"/>
+	  <table:table-row table:style-name="Tabela1.1">
+	   <table:table-cell table:style-name="Tabela1.A1" office:value-type="string">
+		<text:p text:style-name="P7"><office:annotation>
+		  <dc:creator>Ronoaldo JLP</dc:creator>
+		  <dc:date>2018-02-01T20:57:59.666690844</dc:date>
+		  <text:p text:style-name="P9"><text:span text:style-name="T7">{{range .Items}}</text:span></text:p>
+		 </office:annotation>{{.ItemNo}}</text:p>
+	   </table:table-cell>
+	   <table:table-cell table:style-name="Tabela1.A1" office:value-type="string">
+		<text:p text:style-name="Table_20_Contents">{{.Name}}</text:p>
+	   </table:table-cell>
+	   <table:table-cell table:style-name="Tabela1.A1" office:value-type="string">
+		<text:p text:style-name="Table_20_Contents">{{.Quant}}</text:p>
+	   </table:table-cell>
+	   <table:table-cell table:style-name="Tabela1.A1" office:value-type="string">
+		<text:p text:style-name="Table_20_Contents">{{.Value}}</text:p>
+	   </table:table-cell>
+	   <table:table-cell table:style-name="Tabela1.E1" office:value-type="string">
+		<text:p text:style-name="P8">{{.Total}}<office:annotation>
+		  <dc:creator>Ronoaldo JLP</dc:creator>
+		  <dc:date>2018-02-01T20:58:14.379188531</dc:date>
+		  <text:p text:style-name="P9"><text:span text:style-name="T7">{{end}}</text:span></text:p>
+		 </office:annotation></text:p>
+	   </table:table-cell>
+	  </table:table-row>
+	 </table:table>
+	 <text:p text:style-name="P6"/>
+	</office:text>
+   </office:body>`
+
+	output, err := prepareXMLForTemplate(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("output=%v", output)
+}
