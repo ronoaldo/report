0bdefeb4bf438aafc7bf6ae9ffbc42d75309ee2e
Ronoaldo JLP <ronoaldo@gmail.com>
2018-02-08 01:56:43 -0200

    Allow disable verbose output

--- odf/log.go
+++ odf/log.go
@@ -1,8 +1,18 @@
 package odf
 
 import (
+	"flag"
 	"log"
 	"os"
 )
 
 var logger = log.New(os.Stderr, "[odf] ", log.LstdFlags)
+
+var debug = flag.Bool("odf-debug", false, "Allow debugging output to os.Stderr")
+
+func printf(format string, v ...interface{}) {
+	if debug == nil || !*debug {
+		return
+	}
+	logger.Printf(format, v...)
+}
